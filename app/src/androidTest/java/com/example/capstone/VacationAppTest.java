5875e228fc4e08e756092a33879eb165e7b50fbc
jelittle43 <julialittlevital@gmail.com>
2024-02-12 22:23:19 -0500

    Espresso Test Scripts - Add and Edit

--- app/src/androidTest/java/com/example/capstone/VacationAppTest.java
+++ app/src/androidTest/java/com/example/capstone/VacationAppTest.java
@@ -4,9 +4,7 @@ package com.example.capstone;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
 
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -14,14 +12,15 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import com.example.capstone.Activities.VacationList;
 
 
-import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
+
+
 import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.clearText;
+
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -33,6 +32,8 @@ import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
+import android.content.Context;
+
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class VacationAppTest {
 
@@ -40,10 +41,9 @@ public class VacationAppTest {
     public ActivityScenarioRule<VacationList> activityScenarioRule = new ActivityScenarioRule<>(VacationList.class);
 
     @Test
-    public void testCRUDOperations() {
-
-        //Save Vacation
+    public void testSaveVacation() {
         ActivityScenario<VacationList> scenario = ActivityScenario.launch(VacationList.class);
+
         Espresso.onView(withId(R.id.floatingActionButton)).perform(click());
         Espresso.onView(withId(R.id.vacationname)).perform(typeText("Test Vacation"), closeSoftKeyboard());
         Espresso.onView(withId(R.id.hotelname)).perform(typeText("Test Hotel"), closeSoftKeyboard());
@@ -53,23 +53,34 @@ public class VacationAppTest {
         Espresso.onView(withText("Save Vacation")).perform(click());
         Espresso.onView(withId(R.id.recyclerview)).check(matches(isDisplayed()));
         Espresso.onView(withText("Test Vacation")).check(matches(isDisplayed()));
+    }
 
-        //Edit Vacation
+    @Test
+    public void testEditVacation() {
+        ActivityScenario<VacationList> scenario = ActivityScenario.launch(VacationList.class);
+
+        Espresso.onView(withId(R.id.floatingActionButton)).perform(click());
+        Espresso.onView(withId(R.id.vacationname)).perform(typeText("PreEdit Vacation"), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.hotelname)).perform(typeText("Test Hotel"), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.startdate)).perform(typeText("02/12/2024"), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.enddate)).perform(typeText("02/13/2024"), closeSoftKeyboard());
+        Espresso.openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Espresso.onView(withText("Save Vacation")).perform(click());
+        Espresso.onView(withId(R.id.recyclerview)).check(matches(isDisplayed()));
+        Espresso.onView(withText("PreEdit Vacation")).check(matches(isDisplayed()));
+
         Espresso.onView(withId(R.id.recyclerview))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+                .perform(RecyclerViewActions.scrollTo(hasDescendant(withText("PreEdit Vacation"))));
+
+        Espresso.onView(withText("PreEdit Vacation")).perform(click());
         Espresso.onView(withId(R.id.vacationname)).perform(clearText(), typeText("Edited Vacation"), closeSoftKeyboard());
         Espresso.onView(withId(R.id.hotelname)).perform(clearText(), typeText("Edited Hotel"), closeSoftKeyboard());
         openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
         Espresso.onView(withText("Save Vacation")).perform(click());
         Espresso.onView(withId(R.id.recyclerview)).check(matches(isDisplayed()));
         Espresso.onView(withText("Edited Vacation")).check(matches(isDisplayed()));
-
-        //Delete Vacation
-        Espresso.onView(withId(R.id.recyclerview))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
-        Espresso.onView(withText("Delete Vacation")).perform(click());
-        Espresso.onView(withText("Edited Vacation")).check(doesNotExist());
     }
 
 }
+
+
